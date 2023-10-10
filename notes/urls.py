0364f513c7c138213f36a1d40c8255449669b65e
dhan <kedanubalde41@gmail.com>
2023-10-10 11:23:23 +0800

    add

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.TaskList, name="tasks"),
-    path('<str:pk>/', views.TaskDetail, name="task"),
-    path('<str:pk>/delete/', views.TaskDelete, name="delete"),
+    path('', views.TaskList.as_view(), name="tasks"),
+    path('<str:pk>/', views.TaskDetail.as_view(), name="task"),
+    path('<str:pk>/delete/', views.TaskDelete.as_view(), name="delete"),
 ]
